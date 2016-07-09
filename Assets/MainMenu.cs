8861e93c8e4c39f4be07951810773a238fe632e0
ellabahamady <nurlaela.bahamady@gmail.com>
2016-07-09 13:03:27 +0700

    Update

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	public GUISkin MySkin;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnGUI(){
+		float btnWidth = 200;
+		float btnHeight = 35;
+		float btnSpace = 40;
+		float btnLeft = (Screen.width / 2) - 100;
+		float btnTop = 320;
+		GUI.skin = MySkin;
+		if (GUI.Button(new Rect(btnLeft,btnTop,btnWidth,btnHeight),"Start"))
+		{
+			Application.LoadLevel("AR");
+		}
+		if (GUI.Button(new Rect(btnLeft,btnTop+btnSpace,btnWidth,btnHeight),"Tutorial"))
+		{
+			Application.LoadLevel("tutorial");
+		}
+		if (GUI.Button(new Rect(btnLeft,btnTop+(btnSpace*2),btnWidth,btnHeight),"About"))
+		{
+			Application.LoadLevel("about");
+		}
+		if (GUI.Button(new Rect(btnLeft,btnTop+(btnSpace*3),btnWidth,btnHeight),"Quit"))
+		{
+			Application.Quit();
+		}
+	}
+}
---
+++ Assets/tutorial.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tutorial : MonoBehaviour {
+	public GUISkin MySkin;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnGUI(){
+		float btnWidth = 200;
+		float btnHeight = 45;
+		float btnSpace = 50;
+		float btnLeft = (Screen.width / 2) - 100;
+		float btnTop = 400;
+		GUI.skin = MySkin;
+		if (GUI.Button(new Rect(btnLeft,btnTop,btnWidth,btnHeight),"Back"))
+		{
+			Application.LoadLevel("MainMenu");
+		}
+		
+	}
+}
+
